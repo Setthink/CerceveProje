e3097ead058d92a176959b443a409c9b6682f83c
unknown <darkbluee2323@gmail.com>
2023-12-12 15:59:40 +0300

    Added the second cerceveKodu to equation

--- cerceveproje-frontend/src/pages/AddEntityPage.js
+++ cerceveproje-frontend/src/pages/AddEntityPage.js
@@ -26,22 +26,46 @@ const AddEntityPage = () => {
       }, {En: "", Boy:""})
     : {};
 
+  
+  const [camData, setCamData] = useState(null);
+  const [paspartuData, setPaspartuData] = useState(null);  
   const [formData, setFormData] = useState(initialFormData);
   const [cerceveData, setCerceveData] = useState(null);
-  const [siparisFiyat, setSiparisFiyat] = useState(0);
+  const [siparisFiyat, setSiparisFiyat] = useState(0.0);
 
   useEffect(() => {
-    const fetchCerceveData = async () => {
+    const fetchData = async (entityType) => {
       try {
-        const response = await ApiService.getEntityData("cerceve");
-        setCerceveData(response.data);
+        const response = await ApiService.getEntityData(entityType);
+        switch (entityType) {
+          case "cerceve":
+            setCerceveData(response.data);
+            break;
+          case "cam":
+            setCamData(response.data);
+            break;
+          case "paspartu":
+            setPaspartuData(response.data);
+            break;
+          // Add more cases if you have other entities to fetch
+          default:
+            break;
+        }
       } catch (error) {
-        console.error("Error fetching Cerceve data:", error);
+        console.error(`Error fetching ${entityType} data:`, error);
       }
     };
-
-    fetchCerceveData();
+  
+    const fetchAllData = async () => {
+      await fetchData("cerceve");
+      await fetchData("cam");
+      await fetchData("paspartu");
+      // Add more entities if needed
+    };
+  
+    fetchAllData();
   }, []);
+  
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -51,41 +75,111 @@ const AddEntityPage = () => {
     }));
   };
 
- const handleCalculateSiparisFiyat = () => {
-  const cerceveKoduField = entityColumns[entityType].find((column) => column.field === "cerceveKodu");
-  const enField = entityColumns[entityType].find((column) => column.field === "en");
-  const boyField = entityColumns[entityType].find((column) => column.field === "boy");
-  if (cerceveData && cerceveKoduField){
-    const cerceveKoduValue = formData[cerceveKoduField.field];
-    if(cerceveKoduValue){
-      const selectedCerceve = cerceveData.find((cerceve) => cerceve.cerceveKodu === cerceveKoduValue);
-      if(selectedCerceve){
-        console.log("Selected Cerceve:", selectedCerceve);
-        const calculatedFiyat =
-        parseFloat(formData[enField.field]) * parseFloat(selectedCerceve.cerceveFiyat) +
-        parseFloat(formData[boyField.field]) * parseFloat(selectedCerceve.cerceveFiyat);
+  const handleCalculateSiparisFiyat = () => {
+    const cerceveKoduField = entityColumns[entityType].find((column) => column.field === "cerceveKodu");
+    const camKoduField = entityColumns[entityType].find((column) => column.field === "camKodu");
+    const paspartuKoduField = entityColumns[entityType].find((column) => column.field === "paspartuKodu");
+    const enField = entityColumns[entityType].find((column) => column.field === "en");
+    const boyField = entityColumns[entityType].find((column) => column.field === "boy");
+  
+    if (cerceveData && cerceveKoduField && camKoduField && paspartuKoduField) {
+      // Split the values for each field
+      const cerceveKoduValues = formData[cerceveKoduField.field].split(',').map(value => value.trim());
+      const firstCerceve = cerceveData.find((cerceve) => cerceve.cerceveKodu === cerceveKoduValues[0]);
+      const camKoduValues = formData[camKoduField.field].split(',').map(value => value.trim());
+      const paspartuKoduValues = formData[paspartuKoduField.field].split(',').map(value => value.trim());
+  
+      console.log("en:", formData[enField.field]);
+      console.log("boy:", formData[boyField.field]);
+      // Calculate siparisFiyat for cerceveKoduValues
+      const calculatedFiyatCerceve = cerceveKoduValues.reduce((totalFiyat, cerceveKoduValue) => {
+        const selectedCerceve = cerceveData.find((cerceve) => cerceve.cerceveKodu === cerceveKoduValue);
+        if (selectedCerceve) {
+          console.log("Selected Cerceve:", selectedCerceve);
+          const enFiyatCerceve = parseFloat(formData[enField.field]) * parseFloat(selectedCerceve.cerceveFiyat);
+          const boyFiyatCerceve = parseFloat(formData[boyField.field]) * parseFloat(selectedCerceve.cerceveFiyat);
+          console.log("enFiyatCerceve:", enFiyatCerceve);
+          console.log("boyFiyatCerceve:", boyFiyatCerceve);
+          console.log("deneme : ", firstCerceve.cerceveGenislik);
+        
+          const genislikFiyatCerceve =((parseFloat(formData[enField.field]) * parseFloat(selectedCerceve.cerceveGenislik)));
+        
+          console.log("formData[enField.field]:", formData[enField.field]);
+          console.log("formData[cerceveKoduField.field]:", formData[cerceveKoduField.field]);
+          console.log("selectedCerceve.cerceveGenislikFiyat:", selectedCerceve.cerceveGenislik);
+          console.log("genislikFiyatCerceve:", genislikFiyatCerceve);
+        
+          return totalFiyat + enFiyatCerceve + boyFiyatCerceve + genislikFiyatCerceve;
+        }  else {
+          const errorMessage = `Bu koda sahip cerceve bulunamadı: "${cerceveKoduValue}"`;
+          toast.error(errorMessage, {
+            theme: "colored",
+          });
+          console.log(totalFiyat);
+          return totalFiyat;
+        }
+      }, 0);
 
-      console.log("Calculated Siparis Fiyat:", calculatedFiyat);
-      setSiparisFiyat(calculatedFiyat);
-      // Optionally, you can update the form data with the calculated siparisFiyat
+      const calculatedFiyatCam = 31;
+      const calculatedFiyatPaspartu = 32;
+      
+/*       const calculatedFiyatPaspartu = paspartuKoduValues.reduce((totalFiyat, paspartuKoduValue) => {
+        const selectedPaspartu = paspartuData.find((paspartu) => paspartu.paspartuKodu === paspartuKoduValue);
+        if (selectedPaspartu) {
+          // Your logic for paspartu
+          const enFiyatPaspartu = parseFloat(formData[enField.field]) * parseFloat(selectedPaspartu.paspartuFiyat);
+          const boyFiyatPaspartu = parseFloat(formData[boyField.field]) * parseFloat(selectedPaspartu.paspartuFiyat);
+      
+          return totalFiyat + enFiyatPaspartu + boyFiyatPaspartu;
+        } else {
+          const errorMessage = `Bu koda sahip paspartu bulunamadı: "${paspartuKoduValue}"`;
+          toast.error(errorMessage, {
+            theme: "colored",
+          });
+          console.log(totalFiyat);
+          return totalFiyat;
+        }
+      }, 0);
+      
+      // Calculate siparisFiyat for camKoduValues
+      const calculatedFiyatCam = camKoduValues.reduce((totalFiyat, camKoduValue) => {
+        const selectedCam = camData.find((cam) => cam.camKodu === camKoduValue);
+        if (selectedCam) {
+          // Your logic for cam
+          const enFiyatCam = parseFloat(formData[enField.field]) * parseFloat(selectedCam.camFiyat);
+          const boyFiyatCam = parseFloat(formData[boyField.field]) * parseFloat(selectedCam.camFiyat);
+      
+          return totalFiyat + enFiyatCam + boyFiyatCam;
+        } else {
+          const errorMessage = `Bu koda sahip cam bulunamadı: "${camKoduValue}"`;
+          toast.error(errorMessage, {
+            theme: "colored",
+          });
+          console.log(totalFiyat);
+          return totalFiyat;
+        }
+      }, 0); */
+      // Calculate total siparisFiyat by summing up individual calculated values
+      const totalCalculatedFiyat = calculatedFiyatCerceve + calculatedFiyatCam + calculatedFiyatPaspartu;
+  
+      console.log("Total Calculated Siparis Fiyat:", totalCalculatedFiyat);
+      setSiparisFiyat(totalCalculatedFiyat);
       setFormData((prevFormData) => ({
         ...prevFormData,
-        siparisFiyat: calculatedFiyat,
+        siparisFiyat: totalCalculatedFiyat,
       }));
     } else {
-      const errorMessage = `Bu koda sahip çerçeve bulunmamaktadır : "${cerceveKoduValue}"`;
-        // Display toast notification for the error
-        toast.error(errorMessage, {
-          theme: "colored",
-        });
-      }
-  } else {
-    console.log("CerceveKodu is empty");
-  }
-} else {
-  console.log("Invalid entity type or fields for calculating Siparis Fiyat");
-}
-};
+      const errorMessage = "Invalid entity type or fields for calculating Siparis Fiyat";
+      toast.error(errorMessage, {
+        theme: "colored",
+      });
+    }
+  };
+  
+  
+  
+  
+  
   
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -135,6 +229,8 @@ const AddEntityPage = () => {
             fullWidth
             required
             sx={{ my: 2 }}
+            readOnly={column.field === "siparisFiyat"}
+            disabled={column.field === "siparisFiyat"}
           />
         );
       }
